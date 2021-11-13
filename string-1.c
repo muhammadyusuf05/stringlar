dda142470703188c87908ce2bff63a28fa4b4cd8
Muhammadyusuf Abduraimov <muhammadyusufabdurayimov118@gmail.com>
2021-11-13 20:27:12 +0500

    first commit

---
+++ string-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+
+
+
+#include <stdio.h>
+int main()
+{
+    char c;
+
+    printf("Enter n : ");
+    scanf("%s", &c);
+
+    printf("%d", c);
+    printf("\n\n");
+
+    return 0;
+}
+
+
